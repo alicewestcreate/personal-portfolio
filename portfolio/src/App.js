4f56ece2e232b394e5d73706b6a0eaeef01c81be
Alice <alicewest.create@gmail.com>
2023-03-11 19:09:49 +0000

    redo

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,13 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
-import Cage from './Pages/Cage';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {Link} from "react-router-dom";
+import "./App.css";
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Welcome</h1>
+    </div>
+  );
+};
+
+const About = () => {
+  return (
+    <div>
+      <h1>Portfolio Gallery</h1>
+    </div>
+  );
+};
 
 function App() {
   return (
-    <div>
-      <Cage/>
-    </div>
+    <>
+    <h1>Hello</h1>
+    <Link to="/">Home</Link>
+    <Link to="/about">About</Link> 
+    </>    
+
+
   );
 }
 
 export default App;
+
+//   // <Router>
+//   <div>
+//   <h1>Hello</h1>
+//   <Link to="/">Home</Link>
+//   <Link to="/about">About</Link>
+// </div>
+
+//   <Routes>
+//     <Route path="/" exact>
+//       <Home />
+//     </Route>
+
+//     <Route path="/about" exact>
+//       <About/>
+//     </Route>
+//   </Routes>
+// </Router>
